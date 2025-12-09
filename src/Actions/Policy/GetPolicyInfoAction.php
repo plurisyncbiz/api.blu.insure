771e87f84b61ce5b51a41a8abc7822ea07a6fbf8
Plurisync <pierre@plurisync.biz>
2025-12-09 13:22:33 +0200

    GetPolicyInfo - % allocations

--- src/Actions/Policy/GetPolicyInfoAction.php
+++ src/Actions/Policy/GetPolicyInfoAction.php
@@ -42,11 +42,11 @@ class GetPolicyInfoAction extends Action
         //get beneficiaries
         $beneficiaries = $this->policyHolderRepository->getBeneficiariesById($id);
         $beneficiaryCount = count($beneficiaries);
-        return $this->respondWithData($beneficiaryCount, 200, 'Beneficiaries Count');
-        if($beneficiaryCount===2){
-            $percentage_allocation = '50';
-        } elseif($beneficiaryCount === 1) {
-            $percentage_allocation = '100';
+        if ($beneficiaryCount > 0) {
+            // intval drops the decimals completely (e.g., 33.33 becomes 33)
+            $percentage_allocation = intval(100 / $beneficiaryCount);
+        } else {
+            $percentage_allocation = 0;
         }
 
         $beneficiaries_payload = array();
